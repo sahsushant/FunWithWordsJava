8e28631d73e835fb0a2a7dcf25ae537bad5535e0
sahsushant <sahsushant34@gmail.com>
2025-06-09 13:56:33 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        //MAD LIBS GAME
+
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    }
+}
